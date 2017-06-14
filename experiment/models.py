cc4b0842b81652423cf084d82e90d2ed42a3efa4
Sammy Cherna <cherna@mit.edu>
2017-06-14 17:10:32 +0300

    more admin changes

--- experiment/models.py
+++ experiment/models.py
@@ -1,14 +1,33 @@
 from django.db import models
+from datetime import timedelta
 
 class ExperimentCondition(models.Model):
     name = models.CharField('Name of Experiment Condition', max_length=200)
-    num_trials = models.IntegerField('# Trials')
-    p_signal = models.FloatField('Probability of Signal')
-    d_user = models.FloatField("User Sensitivity (d')")
-    d_alert = models.FloatField("Alert Sensitivity (d')")
-    beta_alert = models.FloatField("Alert Criterion (beta)")
-    mean = models.FloatField('Mean')
-    sd = models.FloatField('Standard Deviation')
-    
-
-# Create your models here.
+    num_trials = models.IntegerField('# Trials', default=5)
+    p_signal = models.FloatField('Probability of Signal', default=0.5)
+    v_hit_s = models.FloatField(default=1.0)
+    v_hit_n = models.FloatField(default=1.5)
+    v_miss_s = models.FloatField(default=-1.5)
+    v_miss_n = models.FloatField(default=-1.0)
+    v_cr_s = models.FloatField(default=0.0)
+    v_cr_n = models.FloatField(default=0.0)
+    v_fa_s = models.FloatField(default=0.0)
+    v_fa_n = models.FloatField(default=0.0)
+    d_user = models.FloatField("User Sensitivity (d')", default=1.0)
+    d_alert = models.FloatField("Alert Sensitivity (d')", default=1.0)
+    beta_alert = models.FloatField("Alert Criterion (beta)", default=0.5)
+    mean = models.FloatField('Mean', default=5.0)
+    sd = models.FloatField('Standard Deviation', default=0.5)
+    stimulus_choices = (('num', 'Numbers'), ('rect', 'Rectangles'))
+    stimulus = models.CharField('Choice of Stimulus', max_length=200, choices=stimulus_choices, default='num')
+    alert_color_choices = (('blank','No color'), ('red', 'Red'), ('green', 'Green'))
+    alert_signal_color = models.CharField('Alert Color for Signal', max_length=200, choices=alert_color_choices, default='red')
+    alert_noise_color = models.CharField('Alert Color for Noise', max_length=200, choices=alert_color_choices, default='green')
+    stimulus_duration = models.FloatField('Stimulus Duration (seconds)', default=5.0)
+    stimulus_delay = models.FloatField('Stimulus Delay (seconds)', default=0.0)
+    alert_duration = models.FloatField('Alert Duration (seconds)', default=5.0)
+    alert_delay = models.FloatField('Alert Delay (seconds)', default=0.0)
+    trial_duration = models.FloatField('Total Trial Duration (seconds)', default=5.0)
+    display_last_points = models.BooleanField('Display Points from Last Trial', default=True)
+    display_total_points = models.BooleanField('Display Cumulative Points', default=True)
+    display_num_trial = models.BooleanField('Display Number of Trial in Block', default=True)
