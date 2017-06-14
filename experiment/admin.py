7f9a4237fb6d0ab7f61f340134a9bb0dbc48c7a7
Sammy Cherna <cherna@mit.edu>
2017-06-14 12:51:05 +0300

    beginning of admin page

--- experiment/admin.py
+++ experiment/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import ExperimentCondition
 # Register your models here.
+
+admin.site.register(ExperimentCondition)
--- experiment/models.py
+++ experiment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class ExperimentCondition(models.Model):
+    name = models.CharField('Name of Experiment Condition', max_length=200)
+    num_trials = models.IntegerField('# Trials')
+    p_signal = models.FloatField('Probability of Signal')
+    d_user = models.FloatField("User Sensitivity (d')")
+    d_alert = models.FloatField("Alert Sensitivity (d')")
+    beta_alert = models.FloatField("Alert Criterion (beta)")
+    mean = models.FloatField('Mean')
+    sd = models.FloatField('Standard Deviation')
+    
+
 # Create your models here.
