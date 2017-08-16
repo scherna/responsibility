d98e0a60c3f936798cd9868e12be0ba305b6f325
Sammy Cherna <cherna@mit.edu>
2017-08-16 14:41:12 +0300

    virtualenv

---
+++ responsibility-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/samcherna/Documents/TAU/responsibility/responsibility-env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
